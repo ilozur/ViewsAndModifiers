51b1d5a2ea11d96c3eeb8d0c08ad120c8729f853
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-05 12:19:21 +0300

    What is behind the main SwiftUI view?

--- ViewsAndModifiers/Overview.swift
+++ ViewsAndModifiers/Overview.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct Overview: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.indigo)
+            .foregroundColor(.white)
+            .font(.largeTitle.bold())
     }
 }
 
