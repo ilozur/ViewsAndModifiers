780a956fc9fa4c344149732ceb039b9421f7217f
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-05 12:59:48 +0300

    Environment modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -12,17 +12,20 @@ struct ContentView: View {
     @State private var useRedText = false
     
     var body: some View {
-        if useRedText {
-            Button("Hello, world!") {
-                useRedText.toggle()
-            }
-            .foregroundColor(.red)
-        } else {
-            Button("Hello, world!") {
-                useRedText.toggle()
-            }
-            .foregroundColor(.blue)
+        VStack {
+            Text("Gryffindor")
+                .font(.largeTitle)
+                .blur(radius: 10)
+                .foregroundColor(.red)
+            Text("Hufflepuff")
+                .foregroundColor(.yellow)
+            Text("Ravenclaw")
+                .foregroundColor(.blue)
+            Text("Slytherin")
+                .foregroundColor(.green)
         }
+        .font(.title)
+        .blur(radius: 5)
     }
 }
 
