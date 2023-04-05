ecfd3cccf3dfed79d5a2e186fcdb1f98d19d1b61
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-05 12:46:10 +0300

    Clear Overview

--- ViewsAndModifiers/Overview.swift
+++ ViewsAndModifiers/Overview.swift
@@ -9,27 +9,6 @@ import SwiftUI
 
 struct Overview: View {
     var body: some View {
-        Button(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/) {
-            print(type(of: self.body))
-        }
-            .padding()
-            .background(.red)
-            .padding()
-            .background(.orange)
-            .padding()
-            .background(.yellow)
-            .padding()
-            .background(.green)
-            .padding()
-            .background(.cyan)
-            .padding()
-            .background(.blue)
-            .padding()
-            .background(.purple)
-            .clipShape(RoundedRectangle(cornerRadius: 15))
-            .foregroundColor(.white)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.primary)
     }
 }
 
